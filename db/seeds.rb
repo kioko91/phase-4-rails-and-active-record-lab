2943e44c50cb44987583c1e565ebd34e74aac93d
Calvin <calvinkioko91@gmail.com>
2022-09-27 14:50:36 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starts seeding...🌱"
+students = Student.create([{ first_name: 'Klaus', last_name: 'Michael', grade: 1 }, { first_name: 'Calvin', last_name: 'Kioko', grade: 2 }])
+puts "Done seeding✅"
